80ab4c9a6c0c35dc470162acf51b892c2512c83e
Zinc Glaze <zekegraves@gmail.com>
2019-01-31 20:16:48 -0500

    Updates

---
+++ precache-manifest.8ef23e1584ed1690d37dade97709dc6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e385138ea46c8739b587",
+    "url": "/memory-matrix/static/js/runtime~main.e385138e.js"
+  },
+  {
+    "revision": "1450e8b2c5bfa6e17433",
+    "url": "/memory-matrix/static/js/main.1450e8b2.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/memory-matrix/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "1450e8b2c5bfa6e17433",
+    "url": "/memory-matrix/static/css/main.257a0abe.chunk.css"
+  },
+  {
+    "revision": "192f0fb7d92a3d95014a4b177e93cc2d",
+    "url": "/memory-matrix/index.html"
+  }
+];
